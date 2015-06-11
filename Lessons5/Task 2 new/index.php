ae1ac13f9f7f5cc203fcc7139783b26bd537e381
Andriy Alexandrov <alex.life@gmail.com>
2015-06-11 12:22:03 +0300

    щось наплужив с github

---
+++ Lessons5/Task 2 new/index.php
@@ -0,0 +1,20 @@
+﻿<?php
+if (!isset($_POST['username']))
+{
+$output= 'This for will create the file with your text';
+include 'index.html.php';
+exit();
+}
+
+else if ((isset($_POST['username'])) and (isset($_POST['text'])))
+{
+    $usertext=serialize(array_slice($_POST,0, 1));
+    $fp = fopen('text/'.$_POST['username'].'.txt', "w+");
+    fwrite($fp, $usertext);
+    fclose ($fp);
+    $output= "Your file was create. You can dowload it: <a href='/text/".$_POST['username'].".txt'>by link</a>" ;
+    include 'index.html.php';
+    exit();
+
+}
+
